e64e92212ffe076b834e0646af75e86cd2921122
@ivan_xoxol <ivan_mischenko@niuitmo.ru>
2024-01-26 05:30:07 +0300

    upd mechanisms

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,54 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include "wifi_config.h"
 
 #define BTN_PERIOD 2000
+
+WebServer server(80);
 
 Servo servo;
 
 // Пины обвеса
 const int btn_in_pin = 12;
 const int btn_out_pin = 13;
+
 const int servo_pin = 14;
+
+const int rele_pin = 13;
+
+// Таймер для двери
+uint32_t rele_timer = 0;
 
 // Таймер для сенсоров
 uint32_t btn_timer = 0;
 
+// user_id=333666&name=Ivan&surname=Mischenko&status_enter=out
+void handleData() {
+  String user_id = server.arg("user_id");
+  String name = server.arg("name"); 
+  String surname = server.arg("surname");
+  String status_enter = server.arg("status_enter");
+  if (status_enter == "out") {
+    status_enter = "in";
+    server.send(200, "text/plain", "in");
+  } else {
+    status_enter = "out";
+    server.send(200, "text/plain", "out");
+  }
+  Serial.println("Door opened for " + name + " " + surname + ", status=" + status_enter);
+  // Открываем дверь
+  digitalWrite(rele_pin, HIGH);
+  rele_timer = millis();
+}
+
 void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  // Настраиваем пины обвеса
+  pinMode(rele_pin, OUTPUT);
   pinMode(btn_in_pin, INPUT);
   pinMode(btn_out_pin, INPUT);
   pinMode(servo_pin, OUTPUT);
@@ -24,9 +59,26 @@ void setup() {
   servo.setPeriodHertz(50);             //  standard 50 hz servo
   servo.attach(servo_pin, 500, 2400);   //  pin, min, max impulse
   servo.write(0);                       //  set angle
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+
+  server.on("/data", HTTP_POST, handleData);
+  server.begin();
 }
 
 void loop() {
+  server.handleClient();
+
+  if (millis() - rele_timer >= 5000 && digitalRead(rele_pin)) {
+    digitalWrite(rele_pin, LOW);
+  }
   if (digitalRead(btn_in_pin) && millis() - btn_timer >= BTN_PERIOD) {
     btn_timer = millis();
     for (int pos = 0; pos <= 180; pos++) {
